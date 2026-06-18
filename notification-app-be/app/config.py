780fb8d79130e0ae01f68b207b6201894ec0b207
Bandlasangeeth <sangeethr048@gmail.com>
2026-06-18 13:59:02 +0530

    settings class and rename to AppConfig

--- notification-app-be/app/config.py
+++ notification-app-be/app/config.py
@@ -1,10 +1,16 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings(BaseSettings):
-    EVALUATION_API_BASE_URL: str = "http://4.224.186.213"
-    ACCESS_TOKEN: str = "" # Provide this in .env
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+class AppConfig(BaseSettings):
+    API_BASE_ENDPOINT: str = "http://4.224.186.213"
+    AUTH_TOKEN: str = ""  # Configure this in .env
 
-settings = Settings()
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+
+config = AppConfig()
